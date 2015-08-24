caa965d1d8ebfe53d280254e905fcf2f03cb0373
raphaeldieu <dieuraphael@gmail.com>
2015-08-24 21:48:21 +0800

    add user controller / PUT, INDEX and DESTROY

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def update
+    @user = User.find_by_id(params[:id])
+    if @user.nil?
+      render json: { message: "Cannot find user" }, status: :not_found
+    else
+      @user.update(user_params)
+    end
+  end
+
+  def destroy
+    @user = User.find_by_id(params[:id])
+
+    if @user.nil?
+      render json: { message: "Cannot find user" }, status: :not_found
+    else
+      if @user.destroy
+        render json: { message: "Successfully deleted" }, status: :no_content
+      else
+        render json: { message: "Unsuccessfully deleted" }, status: :bad_request 
+      end
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :age, :occupation, :gender, :is_smoker, :have_kids, :have_pet, :description)
+  end
+end
