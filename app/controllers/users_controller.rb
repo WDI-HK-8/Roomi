808933ff701feb49683628b2922f698d4aa2af94
raphaeldieu <dieuraphael@gmail.com>
2015-08-25 09:32:56 +0800

    add user show in controller and show.jbuilder for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by_id(params[:id])
+
+    if @user.nil?
+      render json: { message: "Cannot find user" }, status: :not_found
+    end
+  end
+
   def destroy
     @user = User.find_by_id(params[:id])
 
